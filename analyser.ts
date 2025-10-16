d8f571d4aa4cc60ce136ea9ccc02842960cc2893
youssef-official <youssef.official.2411@gmail.com>
2025-10-16 23:44:33 +0300

    feat: Add Node.js types and fix analyser type casting
    Includes the addition of `@types/node` for potential Node.js specific development or tooling.
    Also, resolves a TypeScript type casting issue in the `analyser.ts` file by explicitly casting `this.dataArray` to `any` when calling `getByteFrequencyData`.

--- analyser.ts
+++ analyser.ts
@@ -19,7 +19,7 @@ export class Analyser {
   }
 
   update() {
-    this.analyser.getByteFrequencyData(this.dataArray);
+    this.analyser.getByteFrequencyData(this.dataArray as any);
   }
 
   get data() {
